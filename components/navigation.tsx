c9d0dbb571b1cd8219cf55a031e266e56d179307
유현상 <hyunsang@yuhyeonsang-ui-MacBookAir.local>
2024-04-22 17:19:13 +0900

    first commit

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,7 +1,7 @@
 "use client"; 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-
+import styles from "../styles/navigation.module.css"
 
 export default function Navigation() {
 
@@ -9,7 +9,7 @@ export default function Navigation() {
 
     
     return (
-        <nav>
+        <nav className={styles.nav}>
             <ul>
                 <li>
                     <Link href='/'>Home</Link> {path === "/" ? "❤️‍🔥" : ""}
